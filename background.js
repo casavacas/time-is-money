418be775b2f61c5be8cd6296a8bb8ea8ab4981b6
casavacas <henry.ellist@gmail.com>
2016-02-20 09:38:11 -0500

    thank you chris

--- background.js
+++ background.js
@@ -1,1 +1,8 @@
-document.getElementById("submit").addEventListener("click", function(){});
+document.getElementById("submit").addEventListener("click", function(){
+	console.log("listening");
+	var wage = document.forms["inputForm"]["wage"].value;
+	console.log(wage);
+	chrome.storage.local.set({'wage': wage}, function() {
+		console.log('settings saved');
+	});
+});

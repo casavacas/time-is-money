c13f46075f11c8908e58bcea7f5c53b462f15a7e
casavacas <henry.ellist@gmail.com>
2016-02-20 22:56:40 -0500

    henry branch

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
+var dataObj = {'wage': 10.00};
 document.getElementById("submit").addEventListener("click", function(){
 	console.log("listening");
 	var wage = document.forms["inputForm"]["wage"].value;
 	console.log(wage);
 	chrome.storage.local.set({'wage': wage}, function() {
+		dataObj['wage'] = wage;
 		console.log('settings saved');
 	});
 });

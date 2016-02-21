5f3a81ffe3dee3888f5ea46fd1901c3eebaba219
casavacas <henry.ellist@gmail.com>
2016-02-20 22:21:14 -0500

    looping all finds

--- content_script.js
+++ content_script.js
@@ -3,35 +3,45 @@ console.log("print 2");
 $(function() {
 	chrome.storage.local.get('wage', function(items) {
 		console.log(items);
-	});
-	var searchedText = '$';
-	console.log("print 3");
-	var foundin = $('div:contains("'+searchedText+'")');
-	foundin.each( function( i, element){
-		console.log(i);
-		console.log(element);
-		$(this).css("font-weight", "Bold");
-		var str = $(this)[0].innerHTML;
-		console.log(str);
-		console.log(str.indexOf('$'));
-		console.log(str.indexOf('.'));
-		console.log(str.indexOf('.') - str.indexOf('$'));
-		if (str.indexOf('$') >= 0 && str.indexOf('.') >= 0){
-			var i = str.indexOf('.') - str.indexOf('$');
+		var wage = parseFloat(items.wage);
+		//console.log(wage);
+		var searchedText = '$';
+		//console.log("print 3");
+		var foundin = $('div:contains("'+searchedText+'")');
+		foundin.each( function( i, element){
 			console.log(i);
-			if (i < 10 && i >=1){
-				console.log(true);
-				$(this).css('text-decoration', 'underline');
-				var numer = str.substring(str.indexOf('$')+1, str.indexOf('.'));
-				var a = parseInt(numer);
-				console.log(a);
-				var dec = str.substring(str.indexOf('.')+1, str.indexOf('.')+3);
-				console.log(dec);	
-				var d = parseInt(dec);
-				var ans = a+d/100;
-				console.log (ans);
+			//console.log(element.);
+			var str = $(this)[0].innerHTML;
+			//console.log(str);
+			//console.log(str.indexOf('$'));
+			//console.log(str.indexOf('.'));
+			//console.log(str.indexOf('.') - str.indexOf('$'));
+			var str1 = str;
+			while (str.indexOf('$' >= 0 && str.indexOf('.') >= 0)){
+				var i = str.indexOf('.') - str.indexOf('$');
+				//console.log(i);
+				if (i < 10 && i >=1){
+					//console.log(true);
+					var numer = str.substring(str.indexOf('$')+1, str.indexOf('.'));
+					var a = parseInt(numer);
+					console.log(a);
+					var dec = str.substring(str.indexOf('.')+1, str.indexOf('.')+3);
+					//console.log(dec);	
+					var d = parseInt(dec);
+					var ans = a+d/100;
+					//console.log (ans);
+					console.log( Math.round(ans/wage*100)/100.0);
+					console.log("hours");
+					var hours =  Math.round(ans/wage*100)/100.0;
+					str1 	+= str.substring(0, str.indexOf('$')) + hours ;
+					str1 	+= ' hours'+ str.substring(str.indexOf('.')+3, str.length);
+					$(this).html(str1);
+				}
+			}
+			if (str.indexOf('$') >= 0 && str.indexOf('.') >= 0){
 				
 			}
-		}
+		});		
 	});
+	
 });

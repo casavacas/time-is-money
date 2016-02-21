98cf105b6522ee36a3d084618d2a1bd055246c04
Robert Allen <robby.allen24@gmail.com>
2016-02-20 23:07:59 -0500

    folder add?

--- content_script.js
+++ content_script.js
@@ -1,7 +1,6 @@
 $(function load() {
 	//reads from background.js and text prompt input
 	chrome.storage.local.get('wage', function(items) {
-<<<<<<< HEAD
 	
 	//parses through the text body of the document
 	walk(document.body);
@@ -81,50 +80,4 @@ $(function load() {
 
   });
 	
-}); 
-
-=======
-		console.log(items);
-		var wage = parseFloat(items.wage);
-		//console.log(wage);
-		var searchedText = '$';
-		//console.log("print 3");
-		var foundin = $('div:contains("'+searchedText+'")');
-		foundin.each( function( i, element){
-			console.log(i);
-			//console.log(element.);
-			var str = $(this)[0].innerHTML;
-			//console.log(str);
-			//console.log(str.indexOf('$'));
-			//console.log(str.indexOf('.'));
-			//console.log(str.indexOf('.') - str.indexOf('$'));
-			var str1 = str;
-			while (str.indexOf('$' >= 0 && str.indexOf('.') >= 0)){
-				var i = str.indexOf('.') - str.indexOf('$');
-				//console.log(i);
-				if (i < 10 && i >=1){
-					//console.log(true);
-					var numer = str.substring(str.indexOf('$')+1, str.indexOf('.'));
-					var a = parseInt(numer);
-					console.log(a);
-					var dec = str.substring(str.indexOf('.')+1, str.indexOf('.')+3);
-					//console.log(dec);	
-					var d = parseInt(dec);
-					var ans = a+d/100;
-					//console.log (ans);
-					console.log( Math.round(ans/wage*100)/100.0);
-					console.log("hours");
-					var hours =  Math.round(ans/wage*100)/100.0;
-					str1 	+= str.substring(0, str.indexOf('$')) + hours ;
-					str1 	+= ' hours'+ str.substring(str.indexOf('.')+3, str.length);
-					$(this).html(str1);
-				}
-			}
-			if (str.indexOf('$') >= 0 && str.indexOf('.') >= 0){
-				
-			}
-		});		
-	});
-	
 });
->>>>>>> 5f3a81ffe3dee3888f5ea46fd1901c3eebaba219

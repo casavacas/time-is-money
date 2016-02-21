48a150db378dae2372c2f53b0767396f9db40ca1
Nick Morris <nickant04@gmail.com>
2016-02-20 22:50:13 -0500

    walk method

--- background.js
+++ background.js
@@ -1,13 +1,26 @@
 document.getElementById("submit").addEventListener("click", function(){
-<<<<<<< HEAD
 
-    
-=======
 	console.log("listening");
+    //reads input from text button prompt
 	var wage = document.forms["inputForm"]["wage"].value;
 	console.log(wage);
+    //saves data locally
 	chrome.storage.local.set({'wage': wage}, function() {
 		console.log('settings saved');
 	});
->>>>>>> 5d1c727f454c612e42b41ad6092b2eef4cba7b3e
+
+    chrome.tabs.query({
+    active: true,               // Select active tabs
+    lastFocusedWindow: true     // In the current window
+    }, 
+    function(tabs) {
+    // Since there can only be one active tab in one active window, 
+    //  the array has only one element
+    var tab = tabs[0];
+    // Javascript to reload the page
+    var code = 'window.location.reload();';
+    // Execute the code for the current tab
+    chrome.tabs.executeScript(tab.id, {code: code});
+    });
+
 });
--- content_script.js
+++ content_script.js
@@ -1,37 +1,84 @@
-
-console.log("print 2");
-$(function() {
+$(function load() {
+	//reads from background.js and text prompt input
 	chrome.storage.local.get('wage', function(items) {
-		console.log(items);
-	});
-	var searchedText = '$';
-	console.log("print 3");
-	var foundin = $('div:contains("'+searchedText+'")');
-	foundin.each( function( i, element){
-		console.log(i);
-		console.log(element);
-		$(this).css("font-weight", "Bold");
-		var str = $(this)[0].innerHTML;
-		console.log(str);
-		console.log(str.indexOf('$'));
-		console.log(str.indexOf('.'));
-		console.log(str.indexOf('.') - str.indexOf('$'));
-		if (str.indexOf('$') >= 0 && str.indexOf('.') >= 0){
-			var i = str.indexOf('.') - str.indexOf('$');
-			console.log(i);
-			if (i < 10 && i >=1){
-				console.log(true);
-				$(this).css('text-decoration', 'underline');
-				var numer = str.substring(str.indexOf('$')+1, str.indexOf('.'));
-				var a = parseInt(numer);
-				console.log(a);
-				var dec = str.substring(str.indexOf('.')+1, str.indexOf('.')+3);
-				console.log(dec);	
-				var d = parseInt(dec);
-				var ans = a+d/100;
-				console.log (ans);
-				
-			}
+	
+	//parses through the text body of the document
+	walk(document.body);
+
+	//traverses through nodes in the document
+	function walk(node) 
+	{
+
+  		var child, next;
+
+ 		 switch ( node.nodeType )  
+  			{
+    			case 1:  // Element
+   				case 9:  // Document
+    			case 11: // Document fragment
+      			child = node.firstChild;
+    		  while ( child ) 
+      			{
+       				 next = child.nextSibling;
+        			walk(child);
+       				 child = next;
+     			 }
+     				 break;
+
+    			case 3: // Text node
+    			  handleText(node);
+ 			       break;
+  			}
+	}
+
+	function handleText(textNode) 
+	{
+  		var s = textNode.nodeValue;
+ 		var intIndexOfMatch = s.indexOf( "$" ); // was $
+ 		//var intSpaceAfter = s.substring(intIndexOfMatch).indexOf(" ");
+ 		
+
+   		while (intIndexOfMatch != -1){
+    	//console.log(intIndexOfMatch); 
+    	//get the index of the first space following the $ sign
+    	var spaceIndex = intIndexOfMatch + 
+                        s.substring(intIndexOfMatch).indexOf(" "); //add in letters
+       
+    	//var spaceIndex = s.indexOf(" "); 
+    	//console.log(spaceIndex); 
+   		 //get the string of what is between the '$' and the ' ' 
+    	var numString = s.substr((intIndexOfMatch + 1), spaceIndex - 1); 
+   		 //console.log(numString); 
+    	//turn that string into an int
+   		var cost = parseFloat(numString);
+   		// console.log(cost);  
+    	//send cost to hours to pay
+    	var hours = hoursToPay(cost); 
+    	//console.log(hours); 
+    	hours = hours.toString(); 
+    	hours = hours.concat(" hours of work ");  
+    	var stringToRemove = s.substring(intIndexOfMatch, spaceIndex);
+    	s = s.replace( stringToRemove, " " + hours); //had hours in right 
+    
+    
+    	// Get the index of any next matching substring.
+   		intIndexOfMatch = s.indexOf( "$" );
+   		
 		}
-	});
-});
+
+  		textNode.nodeValue = s;
+	}
+
+	//calculates how many wage hours a given price input is
+	function hoursToPay(cost) 
+	{
+    	var hourlyWage = items.wage;
+   		var finalHours = cost/hourlyWage;
+    	var finalHours = finalHours.toFixed(2); 
+    	return finalHours; 
+	}
+
+  });
+	
+}); 
+
